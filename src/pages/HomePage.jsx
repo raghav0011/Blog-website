c37a0fd9017b195f6198a20af6b4b14848776d69
Raghav GC <110081656+raghav0011@users.noreply.github.com>
2023-12-17 23:10:48 +0545

    Initial Commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout';
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainLayout></MainLayout>
+    </div>
+  )
+}
+
+export default HomePage;

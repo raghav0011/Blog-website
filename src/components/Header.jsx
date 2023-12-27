5da4d708636aba1303943ffdde4c69a79b581159
Raghav GC <110081656+raghav0011@users.noreply.github.com>
2023-12-27 16:01:01 +0545

    Added Hero section, Articles seciton and CTA section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,25 +13,30 @@ const NavItemInfo = [
 ];
 
 const NavItem = ({ item }) => {
+  const [dropdown, setDropdown] = React.useState(false);
+
+  const toggleDropdownHandler = () => {
+    setDropdown((curState) => !curState);
+  }
   return (
     <li className="relative group">
-      {item.type == "link" ? (
+      {item.type === "link" ? (
         <>
           <a href="/" className="px-4 py-2">
             {item.name}
           </a>
-          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+          <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
             /
           </span>
         </>
       ) : (
-        <>
-          <a href="/" className="px-4 py-2 flex gap-1 items-center">
+        <div className="flex flex-col items-center">
+          <button className="px-4 py-2 flex gap-1 items-center" onClick={toggleDropdownHandler}>
             <span>{item.name}</span>
             <MdKeyboardArrowDown />
-          </a>
-          <div className="hidden transiotion-all duration-500 pt-4 absolute bottom-0 right-0 tranform translate-y-full group-hover:block w-max">
-            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+          </button>
+            <div className={`${dropdown ? "block" : "hidden"} lg:hidden transiotion-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:tranform lg:translate-y-full lg:group-hover:block w-max`}>
+            <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
               {item.items.map((page) => (
                 <a
                   href="/"
@@ -41,7 +46,7 @@ const NavItem = ({ item }) => {
               ))}
             </ul>
           </div>
-        </>
+        </div>
       )}
     </li>
   );
@@ -57,7 +62,7 @@ const Header = () => {
   };
 
   return (
-    <section>
+    <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img className="w-16" src={images.Logo} alt="logo" />
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MainLayout from '../../components/MainLayout';
+import Hero from './container/Hero';
+import Articles from './container/Articles';
+import CTA from './container/CTA';
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainLayout>
+        <Hero />
+        <Articles />
+        <CTA/>
+      </MainLayout>
+    </div>
+  );
+}
+
+export default HomePage;

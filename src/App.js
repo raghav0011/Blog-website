f0c0b125fed909b480550b75713ba8911ed12c55
Raghav GC <110081656+raghav0011@users.noreply.github.com>
2023-12-30 23:28:09 +0545

    Added react-router, ArticleDetail page , breadcrumbs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/home/HomePage';
+import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
 
 function App() {
   return (
     <div className="App font-opensans">
-      <HomePage />
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
     </div>
   );
 }
